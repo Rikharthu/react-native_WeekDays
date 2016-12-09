9cdb98f41701108661ac4214347a1d3ce86b70ba
ricard.arman.kuodis <ricard.arman.kuodis@accenture.com>
2016-12-09 13:26:07 +0200

    added dynamic styling

--- index.android.js
+++ index.android.js
@@ -1,31 +1,27 @@
-// Import required libraries
-// React and Component from 'react' namespaces
-import React, { Component } from 'react'
+// Import required libraries React and Component from 'react' namespaces
+import React, {Component} from 'react'
 // react-native specific objects from 'react-native'
-import { AppRegistry, View, Text, StyleSheet, Button } from 'react-native'
-// grab our exported component
-// var DayItem = require('day-item')
+import {AppRegistry, View, Text, StyleSheet, Button} from 'react-native'
+// grab our exported component var DayItem = require('day-item')
 import DayItem from './src/day-item'
+var Moment = require('moment');
 
-
-var DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-// 1) Create a react component
+// var DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+// 'Saturday']; 1) Create a react component
 var Weekdays = React.createClass({
-  // Set of instructions that define our component and how it interracts with the user
-  // It's like View in Android
-
-  // Every component must implement a render method
+  // Set of instructions that define our component and how it interracts with the
+  // user It's like View in Android Every component must implement a render method
   /* Assign a new function to a render property
   same as Weekdays.render = new function(){...} */
   render: function () {
-    // The purpose of the render method is to return how the component is going to look like
+    // The purpose of the render method is to return how the component is going to
+    // look like
     /* JSX is a language that compiles it's XML nature into JavaScript
     Example:
       IN:
         <div>Hello {this.props.name}</div>
       OUT:
-        React.createElement("div", null, "Hello ", undefined.props.name); 
+        React.createElement("div", null, "Hello ", undefined.props.name);
       Use https://babeljs.io/repl/ for live preview and converting JSX to JS */
 
     return <View style={styles.container}>
@@ -34,25 +30,28 @@ var Weekdays = React.createClass({
       <Text>
         Days of the week:
       </Text>
-      {/* <DayItem day={DAYS[0]}/> */} 
+      {/* <DayItem day={DAYS[0]}/> */}
       {this.days()}
     </View>
   },
-  
+
   // This function will only be called manually
-  /** Render DayItems for the whole week */
-  days: function(){
-    return DAYS.map(function(day){
-      // will be called 1 time for every element in an array (7 times, 1 for each day)
-      return <DayItem day={day}/>
-      // <DayItem day="Monday"/> . . .
-    })
+  /** Returns an array of DayItems for each day of the week */
+  days: function () {
+    var daysItems = [];
+    for (var i = 0; i < 7; i++) {
+      var day = Moment()
+        .add(i, 'days')
+        .format('dddd');
+      daysItems.push(<DayItem day={day} daysUntil={i}/>)
+    }
+    return daysItems;
   }
 
 })
 
-// 2) Style the React Component
-// styling is made through those objects, syntax and idea similiar to CSS
+// 2) Style the React Component styling is made through those objects, syntax
+// and idea similiar to CSS
 var styles = StyleSheet.create({
   container: {
     flex: 1, // flex - fill up all the space on the screen (weight ind Android)
--- src/day-item.js
+++ src/day-item.js
@@ -4,13 +4,42 @@ import { StyleSheet, Text } from 'react-native'
 
 // 2) Create our component
 var DayItem = React.createClass({
+
     render: function () {
         return (
-        <Text style={styles.day}>
+        // <Text style={styles.day}>
+        <Text style={this.style()}>
             {/* Access set properties */}
             {this.props.day}
         </Text>
         )
+    },
+
+    style: function(){
+        return{
+            color: this.color(),
+            fontWeight:this.fontWeight(),
+            fontSize:this.fontSize(),
+            lineHeight:this.lineHeight()
+        }
+    },
+    
+    color: function(){
+        var opacity = 1/(1+this.props.daysUntil);
+        return 'rgba(0,0,0, '+opacity+');'
+    },
+
+    fontWeight: function(){
+        var weight = 7 - this.props.daysUntil;
+        return ""+weight*100;
+    },
+
+    fontSize: function(){
+        return 60 - 8*this.props.daysUntil;
+    },
+
+    lineHeight: function(){
+        return 70-4*this.props.daysUntil;
     }
 });
 
